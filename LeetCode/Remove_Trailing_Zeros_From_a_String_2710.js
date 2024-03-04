cc17290785e268465192d8c5c883fe09cf255a9d
mazen <mazenmohamed181@gmail.com>
2024-03-04 11:41:16 +0200

    LeetCode: 2710. Remove Trailing Zeros From a String

---
+++ LeetCode/Remove_Trailing_Zeros_From_a_String_2710.js
@@ -0,0 +1,10 @@
+var removeTrailingZeros = function (num) {
+  for (let i = num.length - 1; i >= 0; i--) {
+    if (num[i] == 0) {
+      continue;
+    } else {
+      const extracted = num.slice(0, i + 1);
+      return extracted;
+    }
+  }
+};
